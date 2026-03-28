cd41236bd0baee456ad8d51d1b8ed6de2a2f2aff
maksym-scale <makcumgax5@gmail.com>
2026-03-28 16:31:24 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro:config';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Твоя адреса на GitHub Pages
+  // Вказуємо Astro, що сайт лежить у підпапці
+  base: '/maksym-portfolio', 
   site: 'https://maksym-scale.github.io',
-  // Назва твого репозиторію
-  base: '/maksym-portfolio',
   integrations: [tailwind()],
 });
